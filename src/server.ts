481b86aea13fd89956212b150650ddf4fc571f98
Pamela Badermann <badermann.pamela@gmail.com>
2023-12-30 02:47:25 -0300

    primeiras rotas

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,63 @@
 import express from 'express'
-import dotenv from 'dotenv';
+//import dotenv from 'dotenv';
+import cors from 'cors';
 
 import { Router, Request, Response } from 'express';
 
-dotenv.config();
+//dotenv.config();
 
 const app = express();
 
 const route = Router()
 
+app.listen(5500, () => console.log('Rodando na porta 5500'))
+
+app.use(cors())
+
 app.use(express.json())
 
-console.log("oie")
+let users = [{
+  id: 1,
+  name: "pame"
+}];
 
 route.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'hello world with Typescript' })
-})
+  res.json({ message: users })
+});
 
-app.use(route)
+route.post('/', (req: Request, res: Response) => {
+  const lastId = users[users.length - 1].id
+
+  users.push({
+    id: lastId + 1,
+    name: req.body.name,
+  });
+
+  res.json("saved user")
+});
+
+route.put('/:id', (req: Request, res: Response) => {
+  const userId = req.params.id;
+
+  const user = users.find(user => Number(user.id) === Number(userId))
+
+  if (!user) {
+    return res.json('user not found!')
+  }
+
+  const updatedUser = {
+    ...user,
+    name: req.body.name,
+  }
+
+  users = users.map(user => {
+    if (Number(user.id) === Number(userId)) {
+      user = updatedUser
+    }
+    return user
+  })
+
+  res.json("updated user")
+});
+
+app.use(route);
